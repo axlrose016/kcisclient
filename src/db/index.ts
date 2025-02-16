35a81944b0589daa8bd799e4a7748bfc53659881
Axl Rose Villanueva <axlrose.villanueva.0416@gmail.com>
2025-02-16 23:55:28 +0800

    CUST BEGIN: AXL 02-16-2025 - v8

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,16 +5,16 @@ import  fs from "fs";
 import { seedLibrary } from './utils/migrate';
 
 const DB_FILE = process.env.LOCAL_DB_URL!;
-
+console.log("DB_FILE", DB_FILE)
 const shouldMigrate = !fs.existsSync(DB_FILE);
 const client = createClient({ url: DB_FILE});
 
 export const db = drizzle(client);
 
-if (shouldMigrate) {
-    await import("./utils/migrate")
-      .then((m) => m.migrateDatabase(db))
-      .catch((err) => console.error("Migration failed:", err));
+// if (shouldMigrate) {
+//     await import("./utils/migrate")
+//       .then((m) => m.migrateDatabase(db))
+//       .catch((err) => console.error("Migration failed:", err));
 
-    await seedLibrary(db);
-  }
+//     await seedLibrary(db);
+//   }
