0fb250844ab48e554ea8327c16ffa470fdbda134
Axl Rose G. Villanueva <axlrose.aj.villanueva.016@gmail.com>
2025-02-23 20:33:26 +0800

    CUST BEGIN: AXL 02-23-2025 - v3

--- src/components/service-workers.tsx
+++ src/components/service-workers.tsx
@@ -5,13 +5,17 @@ import { useEffect } from "react";
 export default function ServiceWorker() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/custom-sw.js")
-        .then((registration) => {
-          console.log("Service worker registered with scope", registration.scope);
+      Promise.all([
+        navigator.serviceWorker.register("/custom-sw.js"),
+        navigator.serviceWorker.register("/sw.js"),
+      ])
+        .then((registrations) => {
+          registrations.forEach((registration) => {
+            console.log("Service Worker registered with scope:", registration.scope);
+          });
         })
         .catch((error) => {
-          console.log("Service Worker registration failed: ", error);
+          console.error("Service Worker registration failed:", error);
         });
     }
   }, []);
