a7e229e435c33c622116ac954ecf888a9529bbf9
Axl Rose G. Villanueva <axlrose.aj.villanueva.016@gmail.com>
2025-04-05 04:43:29 +0800

    CUST BEGIN: AXL 04-05-2025
    v3

--- src/app/personprofile/page.tsx
+++ src/app/personprofile/page.tsx
@@ -1,5 +1,4 @@
-import GeneratePDF from "../../components/PDF/CFW-Booklet";
-//import pdfviewer from "../../components/PDF/pdfviewer";
+import GeneratePDF from "@/components/pdf/CFW-Booklet";
 export default function PersonProfileDashboard() {
     return (
         <GeneratePDF/>
