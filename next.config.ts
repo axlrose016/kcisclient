f05f39e82c75b50b035708ff5c4c9cae033e9b7d
Axl Rose G. Villanueva <axlrose.aj.villanueva.016@gmail.com>
2025-06-17 08:34:34 +0800

    DEPLOY BEGIN: AXL 0617-2025
    v3

--- next.config.ts
+++ next.config.ts
@@ -1,70 +1,72 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   fallbacks: {
+    // Failed page requests fallback to this.
     document: "/~offline",
+    // This is for /_next/.../.json files.
     data: "/fallback.json",
+    // This is for images.
     image: "/fallback.webp",
+    // This is for audio files.
     audio: "/fallback.mp3",
+    // This is for video files.
     video: "/fallback.mp4",
+    // This is for fonts.
     font: "/fallback-font.woff2",
   },
   experimental: {
-    reactServerComponents: false,
+    // turbo: {
+    //   enabled: true,
+    // },
+    reactServerComponents: false,  // Disable React Server Components if you're not using them
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self';",
+              "img-src 'self' data:;",
+              "script-src 'self';",
+              "style-src 'self' 'unsafe-inline';",
+            ].join(" "),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 });
 
-const securityHeaders = [
-  // {
-  //   key: "Content-Security-Policy",
-  //   value: `
-  //     default-src 'self';
-  //     script-src https://www.google.com;
-  //     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  //     font-src 'self' https://fonts.gstatic.com;
-  //     img-src 'self' data: https://api.gravserver.com https://www.w3.org/2000/svg;
-  //     frame-src 'self' https://www.google.com;
-  //     object-src 'none';
-  //     frame-ancestors https://www.google.com 'self';
-  //   `.replace(/\s{2,}/g, " ").trim(),
-  // },
-  {
-    key: "Content-Security-Policy",
-    value: [
-      "default-src 'self';",
-      "img-src 'self' data:;",
-      "script-src 'self';",
-      "style-src 'self' 'unsafe-inline';",
-    ].join(" "),
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "no-referrer",
-  },
-  {
-    key: "Cache-Control",
-    value: "public, max-age=31536000, immutable",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-];
-
 module.exports = withPWA({
-  cacheHandler: require.resolve("./cache-handler.js"),
-  cacheMaxMemorySize: 0,
+  cacheHandler: require.resolve('./cache-handler.js'),
+  cacheMaxMemorySize: 0, // disable default in-memory caching,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Disables ESLint during builds
   },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
   env: {
@@ -78,21 +80,105 @@ module.exports = withPWA({
   async rewrites() {
     return [
       {
-        source: "/api-libs/:path*",
-        destination: "https://dxcloud.dswd.gov.ph/api/:path*",
+        source: '/api-libs/:path*',
+        destination: 'https://dxcloud.dswd.gov.ph/api/:path*',
       },
       {
-        source: "/api-kcis/:path*",
-        destination: "https://kcnfms.dswd.gov.ph/kcis/api/:path*",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
+        source: '/api-kcis/:path*',
+        destination: 'https://kcnfms.dswd.gov.ph/kcis/api/:path*',
       },
     ];
   },
 });
+
+
+
+// const withPWA = require("@ducanh2912/next-pwa").default({
+//   dest: "public",
+//   fallbacks: {
+//     document: "/~offline",
+//     data: "/fallback.json",
+//     image: "/fallback.webp",
+//     audio: "/fallback.mp3",
+//     video: "/fallback.mp4",
+//     font: "/fallback-font.woff2",
+//   },
+//   experimental: {
+//     reactServerComponents: false,
+//   },
+// });
+
+// const securityHeaders = [
+//   {
+//     key: "Content-Security-Policy",
+//     value: `
+//       default-src 'self';
+//       script-src https://www.google.com;
+//       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+//       font-src 'self' https://fonts.gstatic.com;
+//       img-src 'self' data: https://api.gravserver.com https://www.w3.org/2000/svg;
+//       frame-src 'self' https://www.google.com;
+//       object-src 'none';
+//       frame-ancestors https://www.google.com 'self';
+//     `.replace(/\s{2,}/g, " ").trim(),
+//   },
+//   {
+//     key: "X-Frame-Options",
+//     value: "DENY",
+//   },
+//   {
+//     key: "X-Content-Type-Options",
+//     value: "nosniff",
+//   },
+//   {
+//     key: "Referrer-Policy",
+//     value: "no-referrer",
+//   },
+//   {
+//     key: "Cache-Control",
+//     value: "public, max-age=31536000, immutable",
+//   },
+//   {
+//     key: "X-XSS-Protection",
+//     value: "1; mode=block",
+//   },
+// ];
+
+// module.exports = withPWA({
+//   cacheHandler: require.resolve("./cache-handler.js"),
+//   cacheMaxMemorySize: 0,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   env: {
+//     NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL,
+//     NEXT_PUBLIC_API_PIMS_BASE_URL: process.env.API_PIMS_BASE_URL,
+//     NEXT_SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY,
+//     NEXT_PUBLIC_DXCLOUD_KEY: process.env.NEXT_PUBLIC_DXCLOUD_KEY,
+//     NEXT_PUBLIC_DXCLOUD_URL: process.env.NEXT_PUBLIC_DXCLOUD_URL,
+//     JSON_SECRET_KEY: process.env.JSON_SECRET_KEY,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api-libs/:path*",
+//         destination: "https://dxcloud.dswd.gov.ph/api/:path*",
+//       },
+//       {
+//         source: "/api-kcis/:path*",
+//         destination: "https://kcnfms.dswd.gov.ph/kcis/api/:path*",
+//       },
+//     ];
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: securityHeaders,
+//       },
+//     ];
+//   },
+// });
